6fb2373c44070eb28960490e0db6553aca1a4c94
Mikhail <perechyorknut@gmail.com>
2022-02-13 18:59:54 +0300

    10 lesson

--- 1.js
+++ 1.js
@@ -1,2 +1,23 @@
 'use strict'
-sfdsf
+let money = prompt("Ваш бюджет на месяц");
+let time = prompt("Введите дату в формате YYYY-MM-DD")
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: null,
+    income: [],
+    savings: false
+}
+
+appData.expenses[
+    prompt("Введите обязательную статью расходов в этом месяце")
+    ] =
+    prompt( "Во сколько обойдется?" );
+appData.expenses[
+    prompt("Введите обязательную статью расходов в этом месяце")
+    ] =
+    prompt( "Во сколько обойдется?" );
+
+alert(appData.budget / 30);
